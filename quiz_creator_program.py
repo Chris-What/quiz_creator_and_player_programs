2aeb640cc78d6239656c9ddaa62ce86f28d0abc7
Chris <chriswell.mangahas@gmail.com>
2025-04-09 17:09:47 +0800

    Added third and fourth lines of code in the quiz creator: these lines will overwrite any previous data written in the initiated text file, so every time the user adds a batch of questions in the file, the others will be removed.

--- quiz_creator_program.py
+++ quiz_creator_program.py
@@ -1,10 +1,12 @@
 #1. display message when opening the program
 print("Welcome to Quiz Creator! This program allows you to create questions to be used in a quiz. To start, enter a question below.")
 
-#2. initiate the name of the text file where the quiz questions will go
+#2. initiate the name of the text file where the quiz questions will go to
 filename = "quiz_questions.txt"
 
 #3. overwrite any previous data written in the text files
+with open(filename, "w") as file:
+    pass
 
 #4. initiate the start of the question numbers to be used in the text file for formatting
 
