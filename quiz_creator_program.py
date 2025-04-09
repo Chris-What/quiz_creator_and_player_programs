3f40bb99c6fb6644d98acfbf06c3ea3b59d71e04
Chris <chriswell.mangahas@gmail.com>
2025-04-09 17:03:03 +0800

    First commit; Added pseudocodes for the quiz creator program. These pseudocodes thoroughly explain what each part of the code will do.

---
+++ quiz_creator_program.py
@@ -0,0 +1,15 @@
+#1. display message when opening the program
+
+#2. initiate the name of the text file where the quiz questions will go
+
+#3. overwrite any previous data written in the text files
+
+#4. initiate the start of the question numbers to be used in the text file for formatting
+
+#5. ask the user to enter a question, four choices to choose from, and the correct answer
+
+#6. put the questions and choices in the text file
+
+#7. ask the user if they want to enter another set of questions and choices
+
+#8. display exit message when the user is done with the program
