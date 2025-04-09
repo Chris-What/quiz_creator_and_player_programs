4315157a0d76a7f2f89e65a8e98c4b5503935ffa
Chris <chriswell.mangahas@gmail.com>
2025-04-09 17:18:05 +0800

    Added 6th-17th lines of code in the quiz creator: these lines ask the user to input a quiz question, four choices, and the user must confirm which choice is correct. (side note: slighty fixed pseudocode number 2.)

--- quiz_creator_program.py
+++ quiz_creator_program.py
@@ -12,6 +12,19 @@ with open(filename, "w") as file:
 question_num = 1
 
 #5. ask the user to enter a question, four choices to choose from, and the correct answer
+while True:
+    question = input("Enter a question for the quiz: ")
+    choice_a = input("Enter choice A: ")
+    choice_b = input("Enter choice B: ")
+    choice_c = input("Enter choice C: ")
+    choice_d = input("Enter choice D: ")
+
+    correct_ans = ""
+    while correct_ans not in ["A", "B", "C", "D"]:
+        correct_ans = input("Which choice is the correct answer? (A/B/C/D): ").upper()
+
+        if correct_ans not in ["A", "B", "C", "D"]:
+            print("Invalid input; please choose between A, B, C, or D.")
 
 #6. put the questions and choices in the text file
 
