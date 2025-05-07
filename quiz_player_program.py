ee3d044abef4a258e67fab75771950ded3ba9a1c
Chris <chriswell.mangahas@gmail.com>
2025-05-07 23:36:09 +0800

    Started work on quiz_player_program. Added pseudocodes for each distinct part of the code.

---
+++ quiz_player_program.py
@@ -0,0 +1,11 @@
+#1. initialize colorama
+
+#2. load the json file containing the quiz questions
+
+#3. display welcome message when opening the program
+
+#4. shuffle the questions, and start the quiz
+
+#5. display the final score achieved
+
+#6. display final message when done with the quiz
